01c04d23dba28440ff6d528323b24287c1220d02
marina-faraco <marinacfaraco@gmail.com>
2025-09-03 21:24:52 -0300

    correção de sintaxe

--- spotify_api.py
+++ spotify_api.py
@@ -3,7 +3,7 @@ from spotipy.oauth2 import SpotifyOAuth
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
     client_id="95f93c8ad16e417ebf794a0542f71eb9",
-    client_secret="3da4a2fa42154c60873ff802ed9a9f78",
+    client_secret="-",
     redirect_uri="http://127.0.0.1:8888/callback",
     scope="user-read-playback-state,user-modify-playback-state"
 ))
